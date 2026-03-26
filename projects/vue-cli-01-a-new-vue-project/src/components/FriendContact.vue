a66a23adbf1f1c2109484467e9b9e90210500f11
afgonzalez-mindden <afgonzalez@mindden.com>
2026-03-26 16:05:38 +0100

    finished module 7

---
+++ projects/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toogleDetails">{{ showDetails ? "Hide" : "Show" }} Details</button>
+    <ul v-if="showDetails">
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Email: </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showDetails: false,
+      friend: {
+        id: "alvaro",
+        name: "Alvaro Felipe",
+        phone: "233 423 234",
+        email: "alvaro@local.com",
+      },
+    };
+  },
+  methods: {
+    toogleDetails() {
+      this.showDetails = !this.showDetails;
+    },
+  },
+};
+</script>
+
+<style scope></style>
